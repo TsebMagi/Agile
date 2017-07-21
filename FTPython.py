2d31da0a71276625934ac36690cf8d51d1d9204c
Joe <cpaul@pdx.edu>
2017-07-21 16:07:12 -0700

    Added multiple file upload and download along with changing the transfer type from ASCII to binary so now we can transfer any type of file.
    Also added a progress bar and percentage completion to each file during transfer.  NOTE: this is a little buggy in Pycharm, but it works without issue if you run it in a proper terminal.

--- FTPython.py
+++ FTPython.py
@@ -4,9 +4,11 @@
 # Imports
 import ftplib as ft
 import os
+
 # State vars
 ftp_connection = None
 connection_name = None
+total_bytes_transferred = 0
 
 
 # Connects to the host and updates the global ftp connection.
@@ -20,9 +22,14 @@ def connect(host, port=20, username="", password="", account_info=""):
         print("Could not connect: ", err)
 
 
-# Places a file on the connected server
-def put(file):
-    ftp_connection.storlines("STOR " + os.path.basename(file), open(file, 'r+b'))
+# Places file(s) on the connected server
+def put(files):
+    global total_bytes_transferred
+    for file in files:
+        total_bytes_transferred = 0
+        ftp_connection.storbinary("STOR " + os.path.basename(file), open(file, 'r+b'),
+                                  8192, g_print_progress(file, os.path.getsize(file)))
+        print("")
 
 
 # Change directory (currently this is a relative path)
@@ -30,17 +37,51 @@ def cd(path):
     ftp_connection.cwd(path)
 
 
-# Get a file from the connected server
-def get(file):
-    ftp_connection.retrlines("RETR " + file, open(file, 'w').write)
+# Gets file(s) from the connected server
+def get(files):
+    global total_bytes_transferred
+    for file in files:
+        total_bytes_transferred = 0
+        store_file = open(os.path.basename(file), 'w+b')
+        ftp_connection.retrbinary("RETR " + file,
+                                  g_write_and_print_progress(store_file, file, ftp_connection.size(file)))
+        print("")
+        store_file.close()
+
+
+# Generates and returns the function to write the transferred bytes to
+# a file and print the progress of the file transfer.
+# This generation is necessary to use the returned function as the callback function for the
+# get command, since the callback function is only allowed to take a single argument.
+def g_write_and_print_progress(store_file, filename, file_size):
+    def write_and_print_progress(bytes_transferred):
+        store_file.write(bytes_transferred)
+        print_function = g_print_progress(filename, file_size)
+        print_function(bytes_transferred)
+
+    return write_and_print_progress
+
+
+# Generates and returns the function to calculate and print the progress of a file transfer.
+# The print formatting was found on https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
+# This generates the callback function for the put command.
+def g_print_progress(filename, file_size):
+    def print_progress(bytes_transferred):
+        global total_bytes_transferred
+        total_bytes_transferred = total_bytes_transferred + len(bytes_transferred)
+        percent_done = float(total_bytes_transferred) / float(file_size)
+        print("\r{1}    [{0:10s}] {2:.1f}%".format('#' * int(percent_done * 10), filename, percent_done * 100),
+              end="", flush=True)
+
+    return print_progress
 
 
 # Prints the Basic Menu
 def help_menu():
     print("Options : \n"
           "connect <host [port] username password> \n"
-          "put <filename>\n"
-          "get <filename>\n"
+          "put <filename filename ...>\n"
+          "get <filename filename ...>\n"
           "cd <path>\n"
           "list \n"
           "close \n"
@@ -69,13 +110,15 @@ def parse_input():
         if len(u_input) < 2:
             print("You need to supply a filename to upload")
         else:
-            put(u_input[1])
+            del u_input[0]
+            put(u_input)
 
     elif u_input[0] == "get":
         if len(u_input) < 2:
             print("You need to supply a file to download")
         else:
-            get(u_input[1])
+            del u_input[0]
+            get(u_input)
 
     elif u_input[0] == "cd":
         if len(u_input) < 2:
