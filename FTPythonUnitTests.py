c06d3587d19496df0d2ba7b3ce6cc2b7b3896f79
Doug Whitley <dwhitley@pdx.edu>
2017-08-15 19:08:22 -0700

    Built Out the Connect Unit Tests

--- FTPythonUnitTests.py
+++ FTPythonUnitTests.py
@@ -17,6 +17,18 @@ class ConnectionTestCases(unittest.TestCase):
         FTPython.connect("babbage.cs.pdx.edu", 8000, "test_user", "test")
         self.assertTrue(FTPython.ftp_connection is not None)
 
-    def test_bad_connection(self):
+    def test_bad_port(self):
         FTPython.connect("babbage.cs.pdx.edu", 800, "test_user", "test")
+        self.assertTrue(FTPython.ftp_connection is None)
+
+    def test_bad_server(self):
+        FTPython.connect("bad", 8000, "test_user", "test")
+        self.assertTrue(FTPython.ftp_connection is None)
+
+    def test_bad_password(self):
+        FTPython.connect("babbage.cs.pdx.edu", 8000, "test_user", "bad")
+        self.assertTrue(FTPython.ftp_connection is None)
+
+    def test_bad_username(self):
+        FTPython.connect("babbage.cs.pdx.edu", 8000, "bad", "test")
         self.assertTrue(FTPython.ftp_connection is None)
