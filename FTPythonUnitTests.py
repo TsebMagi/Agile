7a2121c4b9a90f2aed3e78903a57ad1609cb4e0d
Doug Whitley <dwhitley@pdx.edu>
2017-08-17 15:22:00 -0700

    Added Testing for user input parsing

--- FTPythonUnitTests.py
+++ FTPythonUnitTests.py
@@ -1,6 +1,6 @@
 import unittest
 import FTPython
-
+import sys
 
 class ConnectionTestCases(unittest.TestCase):
 
@@ -32,3 +32,18 @@ class ConnectionTestCases(unittest.TestCase):
     def test_bad_username(self):
         FTPython.connect("babbage.cs.pdx.edu", 8000, "bad", "test")
         self.assertTrue(FTPython.ftp_connection is None)
+
+
+class ParseInputTestCases(unittest.TestCase):
+    std_in = None
+
+    def setUp(self):
+        self.std_in = sys.stdin
+
+    def tearDown(self):
+        sys.stdin = self.std_in
+
+    def test_connect_bad_input(self):
+        sys.stdin = open("test_connection_bad_input.txt", 'r')
+        FTPython.parse_input()
+        self.assertRaises(ValueError)
